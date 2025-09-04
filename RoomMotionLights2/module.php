ff629432382add64795386b2f1fa82379c68a6f1
Marc Murczak <mmurczak@gmail.com>
2025-09-04 11:30:17 +0200

    cvbcvbcvbcvb

---
+++ RoomMotionLights2/module.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+class RoomMotionLights2 extends IPSModule
+{
+    public function Create()
+    {
+        parent::Create();
+        // TODO: Properties/Variablen/Timer kommen später
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+        // TODO: Registrierungen etc. folgen später
+    }
+
+    public function GetConfigurationForm(): string
+    {
+        return json_encode([
+            'elements' => [],
+            'actions'  => [],
+            'status'   => []
+        ]);
+    }
+}
